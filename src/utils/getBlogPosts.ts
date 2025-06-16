aed814957d28f5db8a0f601bfda9f3d0834a71a6
500tpig <995375558@qq.com>
2025-06-16 14:34:29 +0800

    feat: 优化获取博客文章的逻辑并引入缓存机制
    - 将 `getBlogPosts` 函数重构为使用缓存的形式，提升性能
    - 确保返回的数据结构符合 `ArticlePost` 类型
    - 增强按月分组的功能，优化代码可读性

--- src/utils/getBlogPosts.ts
+++ src/utils/getBlogPosts.ts
@@ -1,4 +1,5 @@
-// src/utils/getBlogPosts.ts
+import { cache } from 'react' 
+
 import fs from 'fs'
 import path from 'path'
 
@@ -7,54 +8,57 @@ import matter from 'gray-matter'
 
 import { tagsColors } from './tagsColors'
 
-export async function getBlogPosts(): Promise<{
-  posts: ArticlePost[]
-  postsByMonth: PostsByMonth
-}> {
-  const postsDirectory = path.join(process.cwd(), 'posts')
-  let filenames = await fs.promises.readdir(postsDirectory)
-  filenames = filenames.reverse()
+export const getBlogPosts = cache(
+  async (): Promise<{
+    posts: ArticlePost[]
+    postsByMonth: PostsByMonth
+  }> => {
+    // console.log('Fetching and processing all blog posts...') // 可以加这行来验证函数是否被重复执行
+    const postsDirectory = path.join(process.cwd(), 'posts')
+    let filenames = await fs.promises.readdir(postsDirectory)
+    filenames = filenames.reverse()
 
-  const posts = await Promise.all(
-    filenames.map(async filename => {
-      const fullPath = path.join(postsDirectory, filename)
-      const fileContents = await fs.promises.readFile(fullPath, 'utf8')
+    const posts = await Promise.all(
+      filenames.map(async filename => {
+        const fullPath = path.join(postsDirectory, filename)
+        const fileContents = await fs.promises.readFile(fullPath, 'utf8')
 
-      const { data, content } = matter(fileContents)
-      const wordCount = content.split(/\s+/g).length
-      const wordsPerMinute = 200
-      const readingTime = Math.ceil(wordCount / wordsPerMinute)
+        const { data, content } = matter(fileContents)
+        const wordCount = content.split(/\s+/g).length
+        const wordsPerMinute = 200
+        const readingTime = Math.ceil(wordCount / wordsPerMinute)
 
-      // 确保返回的数据结构符合 ArticlePost 类型
-      return {
-        id: filename,
-        slug: filename.replace(/\.(mdx|md)$/, ''),
-        title: data.title,
-        content,
-        color: tagsColors[data.tags.split(',')[0].trim() as keyof typeof tagsColors],
-        metadata: {
+        // 确保返回的数据结构符合 ArticlePost 类型
+        return {
+          id: filename,
+          slug: filename.replace(/\.(mdx|md)$/, ''),
           title: data.title,
-          date: data.date,
-          overview: data.overview,
-          tags: data.tags,
-          readingTime
+          content,
+          color: tagsColors[data.tags.split(',')[0].trim() as keyof typeof tagsColors],
+          metadata: {
+            title: data.title,
+            date: data.date,
+            overview: data.overview,
+            tags: data.tags,
+            readingTime
+          }
         }
+      })
+    )
+
+    // 按月分组，得到时间线的数据
+    const postsByMonth: PostsByMonth = posts.reduce((acc: PostsByMonth, post: ArticlePost) => {
+      const month = dayjs(post.metadata.date).format('YYYY-MM-DD').slice(0, 7)
+      if (!acc[month]) {
+        acc[month] = []
       }
-    })
-  )
+      acc[month].push(post)
+      return acc
+    }, {})
 
-  // 按月分组，得到时间线的数据
-  const postsByMonth: PostsByMonth = posts.reduce((acc: PostsByMonth, post: ArticlePost) => {
-    const month = dayjs(post.metadata.date).format('YYYY-MM-DD').slice(0, 7)
-    if (!acc[month]) {
-      acc[month] = []
+    return {
+      posts,
+      postsByMonth
     }
-    acc[month].push(post)
-    return acc
-  }, {})
-
-  return {
-    posts,
-    postsByMonth
   }
-}
+)
